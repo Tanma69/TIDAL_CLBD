753619804f1dc4485048337220ab17937dbe8ad3
Timothee Levilly <tim.levilly@gmail.com>
2022-02-01 12:00:03 +0100

    mise en page js

---
+++ js/recherche.js
@@ -0,0 +1,12 @@
+window.onload = function gridcss(){
+    let Maladies = document.getElementsByClassName("M");
+    let Symptomes = document.getElementsByClassName("S");
+
+    let n = Maladies.length;
+    let k = 0;
+    while(k<n){
+        Maladies[k].style.gridRow = 4+k;
+        Symptomes[k].style.gridRow = 4+k;
+        k++;
+    }
+}
